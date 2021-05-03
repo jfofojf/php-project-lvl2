416394599c11d94419aeb55e097a0a55a9633027
Igor <vvanow@yandex.ru>
2021-05-03 17:19:27 +0300

    fix :

--- src/Parser.php
+++ src/Parser.php
@@ -14,7 +14,7 @@ function parse($data, string $format)
         case 'json':
             return json_decode($data);
         case 'YAML':
-        case 'yml' :
+        case 'yml':
             return Yaml::parse($data, Yaml::PARSE_OBJECT_FOR_MAP);
         default:
             throw new Exception("format $format not supported");
